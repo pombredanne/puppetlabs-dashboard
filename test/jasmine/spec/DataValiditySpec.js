2ff6e3de83a81a99032c3e9f1f7072e99b8b3c3b
Luis Cañas Díaz <lcanas@bitergia.com>
2014-10-01 10:47:01 +0200

    Revert "Updated to last version of VizGrimoireJS."
    This reverts commit a93303c004e392445edc40745514bae0cb680428.

--- test/jasmine/spec/DataValiditySpec.js
+++ test/jasmine/spec/DataValiditySpec.js
@@ -24,24 +24,19 @@ describe("VizGrimoireJS data validity", function() {
             ds = getDataSource('scm');
             commits = ds.getData().scm_commits;
             authors = ds.getData().scm_authors;
-            expect(ss.sample_correlation(commits,authors)).toBeGreaterThan("0.9");
+            expect(ss.sample_correlation(commits,authors)).toBeGreaterThan("0.85");
         });        
         it("High commits and files", function() {
             ds = getDataSource('scm');
             commits = ds.getData().scm_commits;
             files = ds.getData().scm_files;
-            // expect(ss.sample_correlation(commits,files)).toBeGreaterThan("0.9");
-            // 29/03 it is different: 0.82
-            // 31/03 it is different: 0.78
-            // 12/04 it is different: 0.76
-            // 24/04 it is different: 0.70
-            expect(ss.sample_correlation(commits,files)).toBeGreaterThan("0.70");        
+            expect(ss.sample_correlation(commits,files)).toBeGreaterThan("0.45");        
         });        
         it("Medium added lines and removed lines", function() {
             ds = getDataSource('scm');
             added_lines = ds.getData().scm_added_lines;
             removed_lines = ds.getData().scm_removed_lines;
-            expect(ss.sample_correlation(added_lines,removed_lines)).toBeGreaterThan("0.8");        
+            expect(ss.sample_correlation(added_lines,removed_lines)).toBeGreaterThan("0.2");
         });
     });
     
@@ -50,15 +45,13 @@ describe("VizGrimoireJS data validity", function() {
             ds = getDataSource('its');
             openers = ds.getData().its_openers;
             opened = ds.getData().its_opened;
-            expect(ss.sample_correlation(opened, openers)).toBeGreaterThan("0.8");
+            expect(ss.sample_correlation(opened, openers)).toBeGreaterThan("0.7");
         });
         it("High closers and closed", function() {
             ds = getDataSource('its');
             its_closers = ds.getData().its_closers;
             its_closed = ds.getData().its_closed;
-            // expect(ss.sample_correlation(its_closed, its_closers)).toBeGreaterThan("0.9");
-            // 19/02/2014 0.8379226234646485
-            expect(ss.sample_correlation(its_closed, its_closers)).toBeGreaterThan("0.8");
+            expect(ss.sample_correlation(its_closed, its_closers)).toBeGreaterThan("0.9");
         });
         it("Medium changers and changed", function() {
             ds = getDataSource('its');
